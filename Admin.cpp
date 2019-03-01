2d339c7acdbd58808decb70f740d43b85134e13b
NgocNhi123 <44133684+NgocNhi123@users.noreply.github.com>
2019-03-01 10:46:48 +0700

    Add files via upload

---
+++ Admin.cpp
@@ -0,0 +1,16 @@
+#include"Admin.h"
+
+void Admin::inputAAdmin(int id, string last, string first, int gen, string d) {
+	ID = id;
+
+	lastName = last;
+	firstName = first;
+
+	gender = gen;
+
+	date = d;
+}
+
+void Admin::clear() {
+
+}
